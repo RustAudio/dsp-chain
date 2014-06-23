029a4ab66620c928e116196baeabd989825eacc8
mitchmindtree <mitchell.nordine@gmail.com>
2014-06-24 06:48:33 +1000

    Updated with new modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,33 @@
 #![deny(missing_doc)]
 #![feature(macro_rules)]
 #![feature(phase)]
+#![feature(globs)]
 
 //! A pure Rust audio digital signal processing library for Piston.
 
 extern crate portaudio;
 extern crate time;
 
-pub use SoundStream = sound_stream::SoundStream;
-pub use SoundStreamSettings = sound_stream_settings::SoundStreamSettings;
+pub use Frequency = frequency::Frequency;
+pub use HasFrequency = frequency::HasFrequency;
 pub use Node = node::Node;
 pub use IsNode = node::IsNode;
+pub use Oscillator = oscillator::Oscillator;
+pub use Pitch = pitch::Pitch;
+pub use HasPitch = pitch::HasPitch;
 pub use Signal = signal::Signal;
+pub use SoundStream = sound_stream::SoundStream;
+pub use SoundStreamSettings = sound_stream_settings::SoundStreamSettings;
 pub use Waveform = waveform::Waveform;
 
+mod frequency;
+mod math;
+mod node;
+mod oscillator;
+mod pitch;
 mod port_audio_back_end;
+mod signal;
 mod sound_stream;
 mod sound_stream_settings;
-mod node;
+mod waveform;
 
