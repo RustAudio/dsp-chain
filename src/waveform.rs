944c6dd3c04c02dca5fdee587d96a97e7c135b0f
mitchmindtree <mitchell.nordine@gmail.com>
2014-06-24 06:51:53 +1000

    Started the Waveform module

--- src/waveform.rs
+++ src/waveform.rs
@@ -1,11 +1,17 @@
 
 /// An Oscillator must use one of a variety
 /// of waveform types.
-enum Waveform {
+#[deriving(Clone, Show)]
+pub enum Waveform {
+    /// Sine Wave
     Sine,
+    /// Saw Wave
     Saw,
+    /// Square Wave
     Square,
+    /// Noise
     Noise,
+    /// Noise Walk
     NoiseWalk
 }
 
