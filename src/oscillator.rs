8ae074175d3490f48a364065f9aed592afcaa5c4
mitchmindtree <mitchell.nordine@gmail.com>
2014-06-24 18:19:01 +1000

    made compilable, still a work in progress

--- src/oscillator.rs
+++ src/oscillator.rs
@@ -2,11 +2,12 @@
 use sound_stream_settings::SoundStreamSettings;
 use signal::Signal;
 use node::{ Node, IsNode };
+use envelope::Envelope;
 use frequency::Frequency;
 use frequency::constants::{
     HIGHEST_HZ,
     LOWEST_HZ
-}
+};
 use gaussian::Gaussian;
 use pitch::Pitch;
 use pitch::letter::Letter;
@@ -28,7 +29,7 @@ pub struct Oscillator {
     phase: f32,
     signal: Signal<f32>,
     amplitude: Envelope,
-    Frequency: Envelope,
+    frequency: Envelope,
     gaussian: Gaussian,
     is_playing: bool
 }
@@ -57,7 +58,6 @@ impl Oscillator {
 
     /// Oscillator constructor.
     pub fn new(settings: SoundStreamSettings, waveform: Waveform) -> Oscillator {
-        frequency = amplitude.clone();
         Oscillator {
             node: Node::new(settings),
             waveform: waveform,
@@ -85,8 +85,8 @@ impl Oscillator {
         self.signal.set_val(phase);
         // Set frequency to determine phase advance.
         let mut freq_at_ratio = self.get_freq_at_ratio(ratio);
-        freq_at_ratio = freq_at_ratio * note_freq_multiplier;
-        let advance_per_sample = freq_at_ratio / node.settings.samples_per_sec;
+        freq_at_ratio = freq_at_ratio /* * note_freq_multiplier */ ;
+        let advance_per_sample = freq_at_ratio / self.node.settings.samples_per_sec as f32;
         // Advance phase according to frequency.
         self.update_phase(advance_per_sample);
         // Calculate amplitude for sample at ratio.
@@ -96,7 +96,7 @@ impl Oscillator {
     /// Calculate and return the frequency at
     /// the given ratio.
     pub fn get_freq_at_ratio(&self, ratio: f32) -> f32 {
-        frequency.get_value(ratio) * (HIGHEST_HZ - LOWEST_HZ) + LOWEST_HZ
+        self.frequency.get_value(ratio) * (HIGHEST_HZ - LOWEST_HZ) + LOWEST_HZ
     }
 
     /// Iterate the phase according to frequency
