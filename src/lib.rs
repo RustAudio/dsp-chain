01e7cb41fbbd1073a4ed903594f56af7f9cf1a9a
mitchmindtree <mitchell.nordine@gmail.com>
2015-10-10 12:17:38 +1100

    Fix some documentation formatting

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 //! A generic, fast, audio digital signal processing library.
 //!
 //! There are two primary points of interest:
+//!
 //! 1. The [**Graph** type](./graph/struct.Graph.html) - a directed, acyclic audio DSP graph.
+//!
 //! 2. The [**Node** trait](./node/trait.Node.html) - to be implemented for types used within the
 //!    **Graph**.
 //!
